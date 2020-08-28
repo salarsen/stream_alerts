8a9640fc0285e80ee0806659c2c21533950f53da
Spencer <larsen.spencer@gmail.com>
2020-08-27 18:25:55 -0700

    more

--- SETest/widget.js
+++ SETest/widget.js
@@ -3,12 +3,10 @@ let running = false;
 let tipLocale = 'en-US';
 let tipCurrency = 'USD';
 
-
-
 let animate = async() => {
 
     // running = true;
-    console.log(`Event Length: ${myEvents.length}`);
+    //console.log(`Event Length: ${myEvents.length}`);
 
     const userNameContainer = document.querySelector('#letters');
 
@@ -26,7 +24,7 @@ let animate = async() => {
         duration: 900
             // complete: console.log("Add1"),
     }).add({
-        targets: '.container .text',
+        targets: '.container .char',
         opacity: [0, 1],
         translateX: [40, 0],
         translateZ: 0,
@@ -36,35 +34,35 @@ let animate = async() => {
         offset: '-=600',
         delay: (el, i) => 150 + 25 * i
             // complete: console.log("Add2")
-    }).add({
-        targets: '.container .text, .container .line',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000,
-        complete: () => {
-            console.log(`Animation completed, removing contents and slicing array of length ${myEvents.length}`)
-            myEvents.splice(0, 1);
-            // console.log(`Array now: ${myEvents.length}`);
-            if (myEvents.length > 0) {
-                animate();
-            } else {
-                running = false;
-            }
-        }
+    // }).add({
+    //     targets: '.container .char, .container .line',
+    //     opacity: 0,
+    //     duration: 1000,
+    //     easing: "easeOutExpo",
+    //     delay: 1000,
+    //     complete: () => {
+    //         //console.log(`Animation completed, removing contents and slicing array of length ${myEvents.length}`)
+    //         myEvents.splice(0, 1);
+    //         // console.log(`Array now: ${myEvents.length}`);
+    //         if (myEvents.length > 0) {
+    //             animate();
+    //         } else {
+    //             running = false;
+    //         }
+    //     }
     });
 };
 
 
-setInterval(displayEvents, 3000);
+setInterval(displayEvents, 1500);
 
 function displayEvents() {
     // console.log(`Checking if displayEvents running.`)
-    console.log(`Running: ${running}, events count: ${myEvents.length}`)
+    //console.log(`Running: ${running}, events count: ${myEvents.length}`)
     if (!running && myEvents.length > 0) {
         // console.log(`Display events not running, execute since events.length is greater than 0`)
         running = true;
-        console.log(`running now: ${running}`);
+        //console.log(`running now: ${running}`);
         animate();
         // running = animate();
     }
@@ -98,7 +96,11 @@ window.addEventListener('onEventReceived', function(obj) {
 function stringToAnimatedHTML(s) {
     let stringAsArray = s.split('');
     stringAsArray = stringAsArray.map((letter) => {
-        return `<span class="char text">${letter}</span>`
+        if (letter === " ") {
+            return `<span class="char text">&nbsp;</span>`
+        } else {
+            return `<span class="char text">${letter}</span>`
+        }
     });
     return stringAsArray.join('');
 }
@@ -109,31 +111,35 @@ function processEvent(e) {
 
     out_str = ''
     if (listener === 'follower') {
-        console.log(`follow ${event.name}`);
-        out_str = `<span class="char text">${stringToAnimatedHTML(event.name)}</span><span class="icons icons-S char">S</span>`;
+        //console.log(`follow ${event.name}`);
+        out_str = `${stringToAnimatedHTML(event.name)}<span class="icons icons-S char">S</span>`;
     } else if (listener === 'subscriber') {
         // gifted event
-        if (event.amount === 'gift') {
+        if (event.gifted && !event.isCommunityGift) {
             console.log(`gifted: ${event.sender}`)
-            out_str = `<span class="char text">${stringToAnimatedHTML(event.sender)}</span>${getTier(event.tier)}<span class="icons icons-D char">D</span><span class="char text">${stringToAnimatedHTML(event.name)}</span>`;
-        } else {
-            console.log(`subscribed: ${event.name}`);
-            out_str = `<span class="char text">${stringToAnimatedHTML(event.name)}</span>`;
-        }
-        if (event.gifted) {
+                // <span class="icons icons-D char">D</span><span class="char text">${stringToAnimatedHTML(event.name)}</span>`
+            out_str = `${stringToAnimatedHTML(event.sender + " ")}${getTier(event.tier,true)}${stringToAnimatedHTML("->" + event.name)}`;
+            console.log(getTier(event.tier, true));
+        } else if (event.bulkGifted) {
             console.log(`bulk gifts: ${event}`);
+            out_str = `${stringToAnimatedHTML(event.sender + " x" + String(event.amount)+ " ")}${getTier(event.tier,true)};`
+            console.log(getTier(event.tier, true));
+        } else if (!event.gifted && !event.bulkGifted) {
+            console.log(`subscribed: ${event.name}`);
+            out_str = `${stringToAnimatedHTML(event.name)}${getTier(event.tier,false)}${stringToAnimatedHTML(" x" + String(event.amount))}`;
+            console.log(getTier(event.tier, false));
         }
     } else if (listener === 'host') {
         console.log(`hosted: ${event}`);
     } else if (listener === 'cheer') {
         console.log(`cheer: ${event}`);
-        out_str = `${stringToAnimatedHTML(event.name)}<span class="char text">&nbsp;</span><span class="char text">x</span>${stringToAnimatedHTML(event.amount)}<span class="icons icons-A char">A</span>`;
+        out_str = `${stringToAnimatedHTML(event.name + " x" + String(event.amount))}<span class="icons icons-A char">A</span>`;
     } else if (listener === 'tip') {
         console.log(`tip: ${event}`);
-        out_str = `${stringToAnimatedHTML(event.name)}<span class="char text">&nbsp;</span>${stringToAnimatedHTML(currencify(event.amount))}`;
+        out_str = `${stringToAnimatedHTML(event.name) + " " + currencify(event.amount)}`;
     } else if (listener === 'raid') {
         console.log(`raid: ${event}`);
-        out_str = `${stringToAnimatedHTML(event.name)}<span class="char text">&nbsp;</span><span class="icons icons-H char">H</span><span class="char text">x</span>${stringToAnimatedHTML(event.amount)}`;
+        out_str = `${stringToAnimatedHTML(event.name + " ")}<span class="icons icons-H char">H</span>${stringToAnimatedHTML("x " + String(event.amount))}`;
     }
     // console.log(`Events list length before append: ${myEvents.length}`);
     myEvents.push(out_str);
@@ -145,17 +151,35 @@ function processEvent(e) {
     // userNameContainer.innerHTML = "";
 }
 
-function getTier(tier) {
-    switch (String(tier)) {
-        case '1000':
-            return `<span class="char">Tier 1</span>`;
-        case '2000':
-            return `<span class="char">Tier 2</span>`;
-        case '3000':
-            return `<span class="char">Tier 3</span>`;
-        case 'prime':
-            return `<span class="char">Prime</span>`;
-    }
+function getTier(tier, gift) {
+    return `<span class="char"><img src="https://cdn.streamelements.com/uploads/a2981df4-845d-4cd6-a123-f1e9f9594809.png" alt="Tier 1" class="img-icon"></span>`;
+    if (gift) {
+      switch (String(tier)) {
+          case '1000':
+              //return `<span class="char text">Tier 1</span>`;
+              return `<span class="char"><img src="//cdn.streamelements.com/uploads/a2981df4-845d-4cd6-a123-f1e9f9594809.png" alt="Tier 1" class="img-icon char"></span>`;
+          case '2000':
+              //return `<span class="char text">Tier 2</span>`;
+              return `<img src="//cdn.streamelements.com/uploads/a2981df4-845d-4cd6-a123-f1e9f9594809.png" alt="Tier 1" class="img-icon char">`;
+          case '3000':
+              //return `<span class="char text">Tier 3</span>`;
+              return `<img src="//cdn.streamelements.com/uploads/a2981df4-845d-4cd6-a123-f1e9f9594809.png" alt="Tier 1" class="img-icon">`;
+          case 'prime':
+              return `<span class="char icons icons-F">F</span>`;
+      }
+    } else {
+      switch (String(tier)) {
+          case '1000':
+              return `<span class="char text">Tier 1</span>`;
+          
+          case '2000':
+              return `<span class="char text">Tier 2</span>`;
+          case '3000':
+              return `<span class="char text">Tier 3</span>`;
+          case 'prime':
+              return `<span class="char icons icons-F">F</span>`;
+      }
+    } 
 }
 
 
@@ -167,189 +191,3 @@ const currencify = (a) => {
         return a;
     }
 };
-// let eventsLimit = 5,
-//     userLocale = "en-US",
-//     includeFollowers = true,
-//     includeRedemptions = true,
-//     includeHosts = true,
-//     minHost = 0,
-//     includeRaids = true,
-//     minRaid = 0,
-//     includeSubs = true,
-//     includeGifters = true,
-//     includeTips = true,
-//     minTip = 0,
-//     includeCheers = true,
-//     direction = "top",
-//     minCheer = 0,
-//     giftCount = 0;
-
-// let userCurrency,
-//     totalEvents = 0;
-
-// window.addEventListener('onEventReceived', function(obj) {
-//     if (typeof obj.detail.event.itemId !== "undefined") {
-//         obj.detail.listener = "redemption-latest"
-//     }
-
-//     if (obj.detail.listener.indexOf("-latest") < 0) return;
-//     const listener = obj.detail.listener.split("-")[0];
-//     const event = obj.detail.event;
-
-//     if (listener === 'follower') {
-//         if (includeFollowers) {
-//             console.log('here');
-//             addEvent('follower', 'Follower', event.name);
-//         }
-//     } else if (listener === 'redemption') {
-//         if (includeRedemptions) {
-//             addEvent('redemption', 'Redeemed', event.name);
-//         }
-//     } else if (listener === 'subscriber') {
-//         if (includeSubs) {
-//             if (event.amount === 'gift') {
-//                 addEvent('sub', `Sub gift`, event.name);
-//             } else {
-//                 addEvent('sub', `Sub X${event.amount}`, event.name);
-//             }
-//             if (event.gifted && includeGifters) {
-//                 if (giftCount === 0) {
-//                     addEvent('sub', `Gift X${event.count}`, event.sender);
-//                     giftCount = event.count;
-//                 }
-//                 giftCount--;
-//             }
-
-//         }
-//     } else if (listener === 'host') {
-//         if (includeHosts && minHost <= event.amount) {
-//             addEvent('host', `Host ${event.amount.toLocaleString()}`, event.name);
-//         }
-//     } else if (listener === 'cheer') {
-//         if (includeCheers && minCheer <= event.amount) {
-//             addEvent('cheer', `${event.amount.toLocaleString()} Bits`, event.name);
-//         }
-//     } else if (listener === 'tip') {
-//         if (includeTips && minTip <= event.amount) {
-//             addEvent('tip', event.amount.toLocaleString(userLocale, {
-//                 style: 'currency',
-//                 minimumFractionDigits: 0,
-//                 currency: userCurrency.code
-//             }), event.name);
-//         }
-//     } else if (listener === 'raid') {
-//         if (includeRaids && minRaid <= event.amount) {
-//             addEvent('raid', `Raid ${event.amount.toLocaleString()}`, event.name);
-//         }
-//     }
-// });
-
-// window.addEventListener('onWidgetLoad', function(obj) {
-//     let recents = obj.detail.recents;
-
-//     recents.sort(function(a, b) {
-//         return Date.parse(a.createdAt) - Date.parse(b.createdAt);
-//     });
-
-//     userCurrency = obj.detail.currency;
-//     const fieldData = obj.detail.fieldData;
-//     eventsLimit = fieldData.eventsLimit;
-//     includeFollowers = (fieldData.includeFollowers === "yes");
-//     includeRedemptions = (fieldData.includeRedemptions === "yes");
-//     includeHosts = (fieldData.includeHosts === "yes");
-//     minHost = fieldData.minHost;
-//     includeRaids = (fieldData.includeRaids === "yes");
-//     minRaid = fieldData.minRaid;
-//     includeSubs = (fieldData.includeSubs === "yes");
-//     includeGifters = (fieldData.includeGifters === "yes");
-//     includeTips = (fieldData.includeTips === "yes");
-//     minTip = fieldData.minTip;
-//     includeCheers = (fieldData.includeCheers === "yes");
-//     minCheer = fieldData.minCheer;
-//     direction = fieldData.direction;
-
-//     let eventIndex;
-//     for (eventIndex = 0; eventIndex < recents.length; eventIndex++) {
-//         const event = recents[eventIndex];
-
-//         if (event.type === 'follower') {
-//             if (includeFollowers) {
-//                 addEvent('follower', 'Follower', event.name);
-//             }
-//         } else if (event.type === 'redemption') {
-//             if (includeRedemptions) {
-//                 addEvent('redemption', 'Redeemed', event.name);
-//             }
-//         } else if (event.type === 'subscriber') {
-//             if (includeSubs) {
-//                 if (event.amount === 'gift') {
-//                     addEvent('sub', `Sub gift`, event.name);
-//                 } else {
-//                     if (event.gifted && includeGifters && event.amount === event.count) {
-//                         addEvent('sub', `Gift X${event.amount}`, event.sender);
-//                     }
-//                     addEvent('sub', `Sub X${event.amount}`, event.name);
-//                 }
-//             }
-//             if (event.gifted && includeGifters) {
-//                 if (giftCount === 0) {
-//                     addEvent('sub', `Gift X${event.count}`, event.sender);
-//                     giftCount = event.count;
-//                 }
-//                 giftCount--;
-//             }
-//         } else if (event.type === 'host') {
-//             if (includeHosts && minHost <= event.amount) {
-//                 addEvent('host', `Host ${event.amount.toLocaleString()}`, event.name);
-//             }
-//         } else if (event.type === 'cheer') {
-//             if (includeCheers && minCheer <= event.amount) {
-//                 addEvent('cheer', `${event.amount.toLocaleString()} Bits`, event.name);
-//             }
-//         } else if (event.type === 'tip') {
-//             if (includeTips && minTip <= event.amount) {
-//                 addEvent('tip', event.amount.toLocaleString(userLocale, {
-//                     style: 'currency',
-//                     minimumFractionDigits: 0,
-//                     currency: userCurrency.code
-//                 }), event.name);
-//             }
-//         } else if (event.type === 'raid') {
-//             if (includeRaids && minRaid <= event.amount) {
-//                 addEvent('raid', `Raid ${event.amount.toLocaleString()}`, event.name);
-//             }
-//         }
-//     }
-// });
-
-
-
-
-// function addEvent(type, text, username) {
-//     totalEvents += 1;
-//     const element =
-//         `
-//     <div class="event-container" id="event-${totalEvents}">
-// 		<div class="backgroundsvg"></div>
-//         <div class="event-image event-${type}"></div>
-//         <div class="username-container">${username.toUpperCase()}</div>
-//        <div class="details-container">${text.toUpperCase()}</div>
-//     </div>`;
-//     if (direction === "bottom") {
-//         $('.main-container').append(element);
-//     } else {
-//         $('.main-container').prepend(element);
-//     }
-//     if (totalEvents > eventsLimit) {
-//         removeEvent(totalEvents - eventsLimit);
-//     }
-// }
-
-// function removeEvent(eventId) {
-//     $(`#event-${eventId}`).animate({
-//         height: 0,
-//         opacity: 0
-//     }, 'slow', function() {
-//         $(`#event-${eventId}`).remove();
-//     });
-// }
